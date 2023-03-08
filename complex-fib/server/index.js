ccc053e8c35fbfa7f1ddc4981242d9e91ea9c684
kothariji <kotharidhruv25@gmail.com>
2023-03-08 11:20:41 +0530

    add server code for complex-fib

--- complex-fib/server/index.js
+++ complex-fib/server/index.js
@@ -1,5 +1,6 @@
 const keys = require("./keys");
 
+// express
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -7,3 +8,63 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+
+// postgres
+const { Pool } = require("pg");
+
+const pgClient = new Pool({
+  user: keys.pgUser,
+  host: keys.pgHost,
+  database: keys.pgDatabase,
+  password: keys.pgPassword,
+  port: keys.pgPort,
+});
+
+pgClient.on("connect", (client) => {
+  client
+    .query("CREATE TABLE IF NOT EXISTS values (number INT)")
+    .catch((err) => console.error(err));
+});
+
+// redis
+const redis = require("redis");
+
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000,
+});
+
+const redisPublisher = redisClient.duplicate();
+
+// express routes
+app.get("/", (req, res) => {
+  res.send("Server is running, Yayyy!");
+});
+
+app.get("/values/all", async (req, res) => {
+  const values = pgClient.query("SELECT * FROM values;");
+  res.send((await values).rows);
+});
+
+app.get("/values/current", async (req, res) => {
+  redisClient.hGetAll("values", (err, values) => {
+    res.send(values);
+  });
+});
+
+app.post("/values", async (req, res) => {
+  const index = req.body.index;
+  if (parseInt(index) > 40) {
+    res.status(422).send("Index too high");
+  }
+
+  redisClient.hSet("values", index, "Nothing Yet!");
+  redisPublisher.publish("insert", index);
+  pgClient.query("INSERT INTO values(number) VALUES($1)", [index]);
+  res.send({ working: true });
+});
+
+app.listen(5000, () => {
+  console.log("server listening on port 5000");
+});
